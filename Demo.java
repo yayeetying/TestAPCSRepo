d1433b5d35ce9c8e8ffe9dfa2fff8ec64ffcfaf1
Yaying <yliangli20@stuy.edu>
2020-10-21 21:37:55 -0400

    Added all 4 methods to Demo.java

--- Demo.java
+++ Demo.java
@@ -24,9 +24,9 @@ public class Demo {
     for (int i = 0; i < arr.length; i++) {
       str += "{";
       //looks at the length of the column
-      for (int j = 0; j < arr[0].length; j++) {
+      for (int j = 0; j < arr[i].length; j++) {
         str += Integer.toString(arr[i][j]);
-        if (j < arr[0].length-1) str += ", ";
+        if (j < arr[i].length-1) str += ", ";
       }
       if (i != arr.length-1) str += "}, ";
       else {str += "}";}
@@ -44,19 +44,41 @@ public class Demo {
     return arr;
   }
 
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+    int[][] arr = new int[rows][];
+    for (int i = 0; i < rows; i++) {
+      int j = (int)(Math.random() * (cols+1));
+      arr[i] = new int[j];
+      for (int x = 0; x < j; x++) {
+        arr[i][x] = (int)(Math.random() * (maxValue+1));
+      }
+    }
+    return arr;
+  }
+
   public static void main(String[] args) {
     int num = 5;
     if (args.length != 0) num = Integer.parseInt(args[0]);
     printLoop(num);
 
-    int[] a = new int[5];
-    System.out.println(arrToString(a));
-
-    int[][] b = new int[3][4];
-    //System.out.println(arrayDeepToString(b));
-    System.out.println(arrayDeepToString(b).replace("}, ", "},\n"));
-
-    System.out.println(arrayDeepToString(create2DArray(5,4,4)));
-    System.out.println(arrayDeepToString(create2DArray(6,2,3)));
+    // System.out.println();
+    //
+    // int[] a = new int[5];
+    // System.out.println(arrToString(a));
+    // System.out.println();
+    //
+    // int[][] b = new int[3][4];
+    // //System.out.println(arrayDeepToString(b));
+    // System.out.println(arrayDeepToString(b).replace("}, ", "},\n"));
+    // System.out.println();
+    //
+    // System.out.println(arrayDeepToString(create2DArray(5,4,4)).replace("}, ", "},\n"));
+    // System.out.println();
+    // System.out.println(arrayDeepToString(create2DArray(6,2,3)).replace("}, ", "},\n"));
+    // System.out.println();
+    //
+    // System.out.println(arrayDeepToString(create2DArrayRandomized(6,4,4)).replace("}, ", "},\n"));
+    // System.out.println();
+    // System.out.println(arrayDeepToString(create2DArrayRandomized(4,10,100)).replace("}, ", "},\n"));
   }
 }
